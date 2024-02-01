a7b61cce91bec47f98e67d2aaa10017e4c5419c0
lakshayphogat5 <147271073+lakshayphogat5@users.noreply.github.com>
2024-02-01 23:05:58 +0530

    new file:   __init__.py 	new file:   intro.py

---
+++ intro.py
@@ -0,0 +1,10 @@
+a = 50 # int
+b = 50.3 #float
+c = "Hello" #string
+d = True #boolean
+print(type(a),type(b),type(c),type(d),end="\n") #type return the datatype
+
+def add(a = 0,b = 0):#function defination
+    return a+b
+
+print(add(5,44))#function call
